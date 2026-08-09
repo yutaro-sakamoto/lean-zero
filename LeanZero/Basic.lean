f3f4e76b2174200acefa46907a5dd2afb3b3a943
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-08-09 22:29:01 +0900

    wip: wip

--- LeanZero/Basic.lean
+++ LeanZero/Basic.lean
@@ -25,3 +25,24 @@ def eratosthenes (n : Nat) : Array Nat :=
       if isPrime then some i else none
 
 #guard eratosthenes 10 = #[2, 3, 5, 7]
+
+declare_syntax_cat compClause
+
+syntax "for " term " in " term : compClause
+syntax "if " term : compClause
+syntax "[" term " | " compClause,* "]" : term
+
+macro_rules
+  | `([$t |]) => `([$t])
+  | `([$t | for $x in $xs]) => `(List.map (fun $x => $t) $xs)
+  | `([$t | if $x]) => `(if $x then [$t] else [])
+  | `([$t | $c, $cs,*]) => `(List.flatten [[$t | $cs,*] | $c])
+
+#guard [ x ^ 2 | for x in [1, 2, 3, 4, 5]] = [1, 4, 9, 16, 25]
+
+#guard
+  let lhs := [(x, y) | for x in [1, 2, 3], for y in [4, 5]]
+  let rhs := [(1, 4), (1, 5), (2, 4), (2, 5), (3, 4), (3, 5)]
+  lhs = rhs
+
+#guard [x | for x in [1, 2, 3], if x < 2] =[1]
