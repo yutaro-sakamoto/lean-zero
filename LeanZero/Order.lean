a79b97cc570f8b2fb858f057c950bd70d7854156
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-08-29 18:55:46 +0900

    wip: wip

--- LeanZero/Order.lean
+++ LeanZero/Order.lean
@@ -7,3 +7,17 @@ example (m n : Nat) (h : Nat.succ m = Nat.succ n) : m = n := by
 
 example (m n : Nat) (h : m + 1 = n + 1) : m = n := by
   injection h
+
+variable {l m n : Nat}
+
+theorem Nat_add_right_cancel (h : l + m = n + m) : l = n := by
+  induction m with
+  | zero => simp_all
+  | succ m ih =>
+    have lem : (l + m) + 1 = (n + m) + 1 := calc
+      _ = l + (m + 1) := by ac_rfl
+      _ = n + (m + 1) := by rw [h]
+      _ = (n + m) + 1 := by ac_rfl
+    have : l + m = n + m := by
+      injection lem
+    exact ih this
