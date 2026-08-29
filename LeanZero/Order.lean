1af1ccdae176386d79321d272190eaa74b2e8b4f
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-08-29 18:51:17 +0900

    wip: wip

--- LeanZero/Order.lean
+++ LeanZero/Order.lean
@@ -4,3 +4,6 @@ example (n : Nat) : Nat.succ n ≠ Nat.zero := by
 
 example (m n : Nat) (h : Nat.succ m = Nat.succ n) : m = n := by
   injection h
+
+example (m n : Nat) (h : m + 1 = n + 1) : m = n := by
+  injection h
